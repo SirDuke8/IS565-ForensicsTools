e6e31c6669a480bcfd2eaa232279f9b93ac51999
Cameron Wilson <cwilson@nex.io>
2023-11-29 17:13:32 -0700

    vpc, load balancer, security lockdown

--- creation.tf
+++ creation.tf
@@ -4,6 +4,59 @@ provider "aws" {
   profile                  = "default"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "IS565-VPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "IS565-Public-Subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-2b"
+
+  tags = {
+    Name = "IS565-Private-Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "IS565_IGW"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
 resource "aws_security_group" "main" {
   name        = "main-sg"
   description = "Security Group for IS565 EC2"
@@ -12,14 +65,14 @@ resource "aws_security_group" "main" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["128.187.0.0/16"]
   }
 
   ingress {
     from_port   = 3389
     to_port     = 3389
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["128.187.0.0/16"]
   }
 
   egress {
@@ -51,20 +104,63 @@ resource "aws_launch_template" "main" {
     sudo apt full-upgrade -y
   EOF
   )
-
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
-resource "aws_instance" "main" {
+resource "aws_autoscaling_group" "main" {
+  desired_capacity     = 2
+  max_size             = 5
+  min_size             = 1
+  vpc_zone_identifier  = [aws_subnet.private_subnet.id]  # Use private subnet for instances
   launch_template {
     id      = aws_launch_template.main.id
     version = "$Latest"
   }
 
-  tags = {
-    Name = "IS565-october"
+  tag {
+    key                 = "Name"
+    value               = "IS565-october"
+    propagate_at_launch = true
   }
+}
+
+resource "aws_lb" "main" {
+  name               = "IS565-ALB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.main.id]
+  subnets            = [aws_subnet.public_subnet.id]  # Use public subnet for the Load Balancer
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+
+resource "aws_lb_target_group" "main" {
+  name     = "IS565-TargetGroup"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path        = "/"
+    protocol    = "HTTP"
+    port        = 80
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+  }
+}
+
+resource "aws_autoscaling_attachment" "main" {
+  autoscaling_group_name = aws_autoscaling_group.main.name
+  lb_target_group_arn   = aws_lb_target_group.main.arn
 }
